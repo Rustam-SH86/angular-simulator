8de8379b3f810858b4f77121ee243e25662f0aa2
Rustam-SH86 <rust-shafik@yandex.ru>
2026-06-03 13:38:15 +0400

    loader,ngModel,animation

--- src/app/collection.ts
+++ src/app/collection.ts
@@ -1,27 +1,27 @@
-export class Collection<T> {
+ export class Collection<T> {
   private items: T[];
-
   constructor(items: T[] = []) {
     this.items = items;
-  }
+  };
 
   getAll(): T[] {
-    return this.items;
-  }
+    return this.items = [];
+  };
 
-  getOne(index: number): T | undefined {
-    return this.items[index];
-  }
+  getOne(index:number): T | undefined {
+    return this.items[index]
+  };
 
   clear(): void {
-    this.items = [];
-  }
+    this.items = []
+  };
 
-  delete(index: number): void {
-    this.items.splice(index, 1);
-  }
-
+  delete(index:number): void {
+    this.items.splice(index);
+  };
   replace(index: number, newItem: T): void {
-    this.items[index] = newItem;
-  }
-}
+  this.items[index] = newItem;
+};
+ }
+
+
