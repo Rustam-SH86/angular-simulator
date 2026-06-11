45adfca868692245ac7d08b990e764e933ddd3ff
Rustam-SH86 <rust-shafik@yandex.ru>
2026-06-11 16:00:36 +0400

    create messageService

---
+++ src/message.service/message.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { MessageType } from './message.enums';
+import { Message } from './message.interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class MessageService {
+  private currentId = 0;
+  public openedMessages: Message[] = [];
+
+  public addMessage(type: MessageType, text: string): void {
+    const message: Message = {
+      id: ++this.currentId,
+      type,
+      text,
+    };
+    this.openedMessages.unshift(message);
+
+    setTimeout(() => {
+      this.closeMessage(message.id);
+    }, 5000);
+  }
+
+  public closeMessage(id: number): void {
+    this.openedMessages = this.openedMessages.filter((message) => message.id !== id);
+  }
+}
